60f2339f88d1a7622d1a7f5c2c769d92255e8bce
Lau Josefsen <lejo@cego.dk>
2024-01-25 13:17:51 +0100

    Delete undesired hook and make other hook optional

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-[![Releases](https://img.shields.io/badge/releases-purple)](https://github.com/opentelemetry-php/contrib-auto-laravel/releases)
-[![Issues](https://img.shields.io/badge/issues-pink)](https://github.com/open-telemetry/opentelemetry-php/issues)
-[![Source](https://img.shields.io/badge/source-contrib-green)](https://github.com/open-telemetry/opentelemetry-php-contrib/tree/main/src/Instrumentation/Laravel)
-[![Mirror](https://img.shields.io/badge/mirror-opentelemetry--php--contrib-blue)](https://github.com/opentelemetry-php/contrib-auto-laravel)
-[![Latest Version](http://poser.pugx.org/open-telemetry/opentelemetry-auto-laravel/v/unstable)](https://packagist.org/packages/open-telemetry/opentelemetry-auto-laravel/)
-[![Stable](http://poser.pugx.org/open-telemetry/opentelemetry-auto-laravel/v/stable)](https://packagist.org/packages/open-telemetry/opentelemetry-auto-laravel/)
+This is a clone of the read-only subtree split of https://github.com/opentelemetry-php/contrib-auto-laravel
 
-This is a read-only subtree split of https://github.com/open-telemetry/opentelemetry-php-contrib.
-
-# OpenTelemetry Laravel auto-instrumentation
-
-Please read https://opentelemetry.io/docs/instrumentation/php/automatic/ for instructions on how to
-install and configure the extension and SDK.
-
-## Overview
-Auto-instrumentation hooks are registered via composer, and spans will automatically be created.
-
-## Configuration
-
-The extension can be disabled via [runtime configuration](https://opentelemetry.io/docs/instrumentation/php/sdk/#configuration):
-
-```shell
-OTEL_PHP_DISABLED_INSTRUMENTATIONS=laravel
-```
+The only changes are the disabling of the Console kernel handle hook, and optional disable of the Command::execute hook for "long running" commands.
--- src/ConsoleInstrumentation.php
+++ src/ConsoleInstrumentation.php
@@ -19,45 +19,9 @@ class ConsoleInstrumentation
 {
     public static function register(CachedInstrumentation $instrumentation): void
     {
-        hook(
-            Kernel::class,
-            'handle',
-            pre: static function (Kernel $kernel, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
-                /** @psalm-suppress ArgumentTypeCoercion */
-                $builder = $instrumentation->tracer()
-                    ->spanBuilder('Artisan handler')
-                    ->setSpanKind(SpanKind::KIND_PRODUCER)
-                    ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
-                    ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
-                    ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
-                    ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
-
-                $parent = Context::getCurrent();
-                $span = $builder->startSpan();
-                Context::storage()->attach($span->storeInContext($parent));
-
-                return $params;
-            },
-            post: static function (Kernel $kernel, array $params, ?int $exitCode, ?Throwable $exception) {
-                $scope = Context::storage()->scope();
-                if (!$scope) {
-                    return;
-                }
-
-                $scope->detach();
-                $span = Span::fromContext($scope->context());
-                if ($exception) {
-                    $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
-                    $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
-                } elseif ($exitCode !== Command::SUCCESS) {
-                    $span->setStatus(StatusCode::STATUS_ERROR);
-                } else {
-                    $span->setStatus(StatusCode::STATUS_OK);
-                }
-
-                $span->end();
-            }
-        );
+        if(env('APP_LONG_RUNNING', false)) {
+            return;
+        }
 
         hook(
             Command::class,
