4cff92eb4252a818f7a83aea717a7dff9658f157
Alec Sammon <alec.sammon@paddle.com>
2024-05-22 07:38:49 +0100

    Use configuration to determine min log level

--- src/Watchers/LogWatcher.php
+++ src/Watchers/LogWatcher.php
@@ -6,14 +6,21 @@ namespace OpenTelemetry\Contrib\Instrumentation\Laravel\Watchers;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Log\Events\MessageLogged;
-use Illuminate\Log\LogManager;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\Context\Context;
 
 class LogWatcher extends Watcher
 {
-    /** @var LogManager */
-    private LogManager $logger;
+    private const DEBUG = 1;
+    private const  INFO = 2;
+    private const  NOTICE = 3;
+    private const  WARNING = 4;
+    private const  ERROR = 5;
+    private const  CRITICAL = 6;
+    private const  ALERT = 7;
+    private const EMERGENCY = 8;
+
+    private int $minLogLevel;
 
     /** @psalm-suppress UndefinedInterfaceMethod */
     public function register(Application $app): void
@@ -21,7 +28,11 @@ class LogWatcher extends Watcher
         /** @phan-suppress-next-line PhanTypeArraySuspicious */
         $app['events']->listen(MessageLogged::class, [$this, 'recordLog']);
 
-        $this->logger = $app['log'];
+        $this->minLogLevel = self::INFO;
+        $levelStr = strtoupper(env("OTEL_PHP_LOG_LEVEL", "INFO"));
+        if (defined(self::class.'::'.$levelStr)) {
+            $this->minLogLevel = constant(self::class . '::' . $levelStr);
+        }
     }
 
     /**
@@ -29,7 +40,10 @@ class LogWatcher extends Watcher
      */
     public function recordLog(MessageLogged $log): void
     {
-        if (!$this->logger->isHandling($log->level)) {
+        if (
+            defined(self::class.'::'.strtoupper($log->level)) &&
+            constant(self::class . '::' . strtoupper($log->level)) < $this->minLogLevel
+        ) {
             return;
         }
 
